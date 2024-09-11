4db0924e2ea3e6dededf556bdef18dbbf4050c44
kali <kali@kali>
2024-09-11 16:04:10 +0700

    correct syntax

--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -9,7 +9,7 @@ export default function Product() {
         <main className="w-full grid place-content-center">            
             <div className="flex justify-center items-center">List of Product</div>
             <ul>
-            <Lin href="/products/21" className="text-blue-500">Electronic</Lin>
+            <Link href="/products/21" className="text-blue-500">Electronic</Link>
             <Link href="/products/22" className="text-red-500">Kitchen</Link>
             <Link href="/products/23" className="text-red-500">Bathroom</Link>
             <Link href="/products/24" className="text-red-500">Living</Link>
