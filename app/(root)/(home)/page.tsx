8f1bd95e4d422e0b772ca826213a844bbd55e840
kali <kali@kali>
2024-09-11 11:29:54 +0700

    add new line in home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Homepage() {
   return (
     <main className="w-full text-center text-blue-500">
-      Home Page
+      This is Home Page
     </main>
   )
 }
